f97fa786316e3086736d3629adfa678d1b127cf9
Tristan B. Kildaire (BlackAdder) <deavmi@disroot.org>
2015-12-03 16:29:46 +0200

    :)

---
+++ figWeb.sh
@@ -0,0 +1,24 @@
+function generateHomeLoop
+{
+
+	while [ 1 = 1 ]
+	do
+
+		page="<html>
+		<head>
+			<title>figWeb for figNET on $nodeName</title>
+		</head>
+
+		<body><center><font face=\"Liberation Sans\">
+
+			<h1>$nodeName</h1>
+			<h3>Running on figNET <i>v$figNET_VERSION</i></h3>
+			<hr>
+
+		</font></center></body>
+
+</html>"
+
+	done
+
+}
