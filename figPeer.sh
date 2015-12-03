3d565916276299a6a1bd585f045ca5f71a713527
Tristan B. Kildaire (BlackAdder) <deavmi@disroot.org>
2015-12-03 20:48:18 +0200

    Auto-peering is basically done now

--- figPeer.sh
+++ figPeer.sh
@@ -1,5 +1,12 @@
 #figPeer
 
+function addPeer
+{
+
+	echo $node >> nodes.list
+
+}
+
 function addMyself
 {
 
@@ -18,4 +25,21 @@ function addMyself
 	then
 		echo $publicIP:$publicPort >> data/.peers
 	fi
+}
+
+function findPeers
+{
+	myNodes=$(cat nodes.list)
+	remoteNodes=$(cat data/.peers)
+	for node in $myNodes
+	do
+		for remoteNode in $remoteNodes
+		do
+			if [ ! $node = $remoteNode ]
+			then
+				addPeer $remoteNode
+			fi
+		done
+	done
+
 }
