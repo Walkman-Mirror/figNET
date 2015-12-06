bb4925eb6105b3a963261713fb11a40a6b2c8d67
Tristan B. Kildaire (BlackAdder) <deavmi@disroot.org>
2015-12-06 11:53:18 +0200

    Worked on dedication message.

--- figInfo.sh
+++ figInfo.sh
@@ -3,5 +3,7 @@ figNETSuiteVersion="0.0.1"
 figNETSuiteRelease="Awesome Alpha"
 
 #figNET Developer Info
-dedicationMessage="I'd like to dedicate this software to the Kildaire family and especially my faith Catholicism.
+dedicationMessage="I'd like to dedicate this software to the my family and especially my faith Catholicism.
+
+I hope the world can just have peace and that everyone can live a beautiful life with no hatred but just love for each-other and happiness.
 "
