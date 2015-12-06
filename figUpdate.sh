85307de213ccbb4cd1bef18d293a467ad447978a
Tristan B. Kildaire <tristankildaire@gmail.com>
2015-12-06 19:54:47 +0200

    Create figUpdate.sh

---
+++ figUpdate.sh
@@ -0,0 +1,11 @@
+#figNET Software Suite Updater
+#Written by: Tristan B. Kildaire (Deavmi) - (deavmi@disroot.org)
+#License: GNU GPL v3
+#Crowbar Developments
+
+function updateFiles
+{
+
+    echo "todo"
+
+}
