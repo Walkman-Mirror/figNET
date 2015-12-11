cdf6e8a2634cb5946c6c3fed4d595bf644736943
Tristan B. Kildaire <tristankildaire@gmail.com>
2015-12-11 15:23:43 +0200

    Update figUpdate.sh

--- figUpdate.sh
+++ figUpdate.sh
@@ -16,16 +16,17 @@ function updateFiles
 
     #Download all files from GitHub Raw Viewer Storage
     root="https://raw.githubusercontent.com/figNET/figNET/master"
-    wget $root/figAPI.sh --quiet
-    wget $root/figAutoPeer.sh --quiet
-    wget $root/figCTL.sh --quiet
-    wget $root/figCleaner.sh --quiet
-    wget $root/figInfo.sh --quiet
-    wget $root/figNode.sh --quiet
-    wget $root/figQuote.sh --quiet
-    wget $root/figUpdate.sh --quiet
-    wget $root/figWeb.sh --quiet
-    wget $root/figgy.sh --quiet
-    wget $root/quotes.list --quiet
+    wget $root/{figAPI.sh,figAutoPeer.sh,figCTL.sh,figCleaner.sh,figInfo.sh,figNode.sh,figQuote.sh,figUpdate.sh,figWeb.sh,figgy.sh,quotes.list} --quiet
+    #wget $root/figAPI.sh --quiet
+    #wget $root/figAutoPeer.sh --quiet
+    #wget $root/figCTL.sh --quiet
+    #wget $root/figCleaner.sh --quiet
+    #wget $root/figInfo.sh --quiet
+    #wget $root/figNode.sh --quiet
+    #wget $root/figQuote.sh --quiet
+    #wget $root/figUpdate.sh --quiet
+    #wget $root/figWeb.sh --quiet
+    #wget $root/figgy.sh --quiet
+    #wget $root/quotes.list --quiet
 
 }
