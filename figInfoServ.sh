d28281595ffc6555e824c780e3527164c9e76ed2
Tristan B. Kildaire <tristankildaire@gmail.com>
2015-12-12 18:32:42 +0200

    Update figInfoServ.sh

--- figInfoServ.sh
+++ figInfoServ.sh
@@ -6,3 +6,13 @@ function serveInfoServer
     webfsd -i $infoServerIP -p $infoServerPort
 
 }
+
+function checkISEnv
+{
+
+    if [ ! -d "infoServ/" ]
+    then
+        mkdir "infoServ/"
+    fi
+
+}
